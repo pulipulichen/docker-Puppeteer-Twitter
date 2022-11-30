7d0602a81fe5726103b40dbc51039b1c671222fe
Pulipuli Chen <pulipuli.chen@gmail.com>
2022-11-30 23:13:01 +0800

    Wed Nov 30 11:13:01 PM CST 2022

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,31 @@
 #Specify the version of nodejs.
-FROM zenato/puppeteer-renderer
+# FROM zenato/puppeteer-renderer
 #FROM dayyass/muse_as_service:1.1.2
+FROM buildkite/puppeteer
 
+RUN rm /etc/apt/sources.list.d/google.list
 RUN apt-get update
 
 # RUN cat /etc/os-release
 
 RUN apt-get install -y \
-    fonts-noto-cjk
+    fonts-noto-cjk locales
 
 RUN locale-gen zh_TW.UTF-8  
 ENV LC_ALL=zh_TW.UTF-8
 
 RUN mkdir -p /app/
+RUN mkdir -p /1.input/
+RUN mkdir -p /2.output/
+
+RUN npm install text-from-image@1.1.1
+RUN npm install smartcrop@2.0.5
+RUN npm install smartcrop-gm@2.0.2
+RUN npm install gm@1.25.0
+
+# COPY package.json /
+
+# WORKDIR /app
+# RUN npm i
 
 CMD ["node", "/app/index.js"]
--- app/index.js
+++ app/index.js
@@ -1,6 +1,11 @@
 const puppeteer = require('puppeteer');
 
+const ReadText = require('text-from-image');
+
+/*
 (async () => {
+  // console.log('ok')
+  // return false
   const browser = await puppeteer.launch({
     //headless: false,
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--window-size=800,600'],
@@ -8,8 +13,19 @@ const puppeteer = require('puppeteer');
 
   });
   const page = await browser.newPage();
-  await page.goto("https://blog.pulipuli.info/");
-  await page.screenshot({ path: '/app/output/example.png' });
+  // await page.goto("https://blog.pulipuli.info/");
+  // https://wiki.debian.org/ChangeLanguage
+  await page.goto("https://wiki.debian.org/ChangeLanguage");
+
+  let imagePath = '/2.output/example.png'
+  await page.screenshot({ path: imagePath });
+
+  // 可行 20221130-2307 
+  console.log(await ReadText(imagePath));
 
   await browser.close();
 })();
+*/
+
+// smartcrop.crop(image, options)
+
